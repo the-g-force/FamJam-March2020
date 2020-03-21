480abfbdcb7ef467af1009c8096626cb73cb276e
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-03-21 19:45:19 -0400

    Add about page

--- menu/main menu.gd
+++ menu/main menu.gd
@@ -1,10 +1,8 @@
 extends CanvasLayer
 
 signal start_game
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
+var AboutPage = preload("res://menu/AboutPage.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +13,10 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
 func _on_StartButton_pressed():
 	emit_signal("start_game")
+
+
+func _on_HelpButton_pressed():
+	var about_page = AboutPage.instance()
+	add_child(about_page)
