7a99aea48eb9d578df4e1767c8051274e9937c1e
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-03-21 19:12:26 -0400

    Reconfigure cooldown timer

--- level/Attack.gd
+++ level/Attack.gd
@@ -4,18 +4,19 @@ extends TextureButton
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var _available = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func _pressed():
-	if _available:
-		_available = false
-		disabled = true
-		yield(get_tree().create_timer(0.5), 'timeout')
-		_available = true
-		disabled = false
+
 #func _process(delta):
 #	pass
+
+
+func _on_Mushroom_can_shoot():
+	disabled = false
+
+
+func _on_Mushroom_cannot_shoot():
+	disabled = true
