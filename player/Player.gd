e420bff385e3073858c61b307812e67cd7e7ed66
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-03-21 14:14:40 -0400

    Add alternate attack buttons

--- player/Player.gd
+++ player/Player.gd
@@ -40,3 +40,24 @@ func damage():
 
 func _on_death():
 	queue_free()
+
+
+func _on_Defense_pressed():
+	if cooldown == false:
+		var bullet = PlayerBullet.instance()
+		bullet.is_offense = false
+		get_parent().add_child(bullet)
+		bullet.position = position
+		cooldown = true
+		yield(get_tree().create_timer(0.5), 'timeout')
+		cooldown = false
+
+
+func _on_Attack_pressed():
+	if cooldown == false:
+		var bullet = PlayerBullet.instance()
+		get_parent().add_child(bullet)
+		bullet.position = position
+		cooldown = true
+		yield(get_tree().create_timer(0.5), 'timeout')
+		cooldown = false
