b2cbea1bde4f06f2679a758f444d59bfbf366d84
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-03-21 10:43:11 -0400

    Add shot timer

--- player/Player.gd
+++ player/Player.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var cooldown = false
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,7 +12,6 @@ var PlayerBullet = preload("res://player/PlayerBullet.tscn")
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -22,12 +22,19 @@ func _draw():
 
 func _input(event):
 	if event.is_action("shoot_offense"):
-		var bullet = PlayerBullet.instance()
-		get_parent().add_child(bullet)
-		bullet.position = position
+		if cooldown == false:
+			var bullet = PlayerBullet.instance()
+			get_parent().add_child(bullet)
+			bullet.position = position
+			cooldown = true
+			yield(get_tree().create_timer(0.5), 'timeout')
+			cooldown = false
 	if event.is_action("shoot_defense"):
-		var bullet = PlayerBullet.instance()
-		bullet.is_offense = false
-		get_parent().add_child(bullet)
-		bullet.position = position
-		
+		if cooldown == false:
+			var bullet = PlayerBullet.instance()
+			bullet.is_offense = false
+			get_parent().add_child(bullet)
+			bullet.position = position
+			cooldown = true
+			yield(get_tree().create_timer(0.5), 'timeout')
+			cooldown = false
